1b5f26d1856db9d5e75624c9577c1ffbfeb10cb5
shiorii17 <vanshi1711.as@gmail.com>
2024-01-25 18:25:09 -0800

    i made a change?

--- Sierpinski.java
+++ Sierpinski.java
@@ -1,5 +1,6 @@
 public class Sierpinski {
     public static void main(String[] args) {
         System.out.println("************\n**********\n******\n***\n*");
+        System.out.println("change");
     }
 }
