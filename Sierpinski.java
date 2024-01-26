1322f44a434bc4998d199d491d7bb1628f0710c6
shiorii17 <vanshi1711.as@gmail.com>
2024-01-26 13:58:30 -0800

    finished the documentation + closed the scanner to prevent the leak (or whatever) Also can you go over the method description just in case?

--- Sierpinski.java
+++ Sierpinski.java
@@ -1,9 +1,42 @@
+/**
+ * The objective of our program is to draw a Sierpinski's Triangle with a predetermined depth but customizable length, using recursion. 
+ * @author Jay Motamarry 
+ * @author Vanshika Sharma
+ * @since  Jan 30th, 2024
+ * 
+ * Static variables used: 
+ * char[][] triangle - to draw the triangle 
+ * int depth - to determine the number of iterations of the recursive method.
+ * 
+ * Helper Method 'drawHorizontalLine' takes in three ints as its parameters to draw a horizontal line
+ * @param row    an int that gives the value of the row of the triangle where a horizontal line should be drawn.
+ * @param col    an int that gives the value of the column of the triangle where a horizontal line should be drawn.
+ * @param length an int that gives the value of the length of the horizontal line. 
+ * @return void  it changes the static variable 'triangle' and makes a horizontal line of '*' as required.
+ * 
+ * Helper Method 'makeTriangle' makes a triangle using the 'drawHorizontalLine' method.
+ * @param   sc    a Scanner, to be able to get the input of the length of the triangle from the user.
+ * @return  void  it changes the static variable 'triangle' and makes a triangle with '*' as required.\
+ * 
+ * Helper Method 'printTriangle' prints out the triangle to the user as an output.
+ * @return void   displays an image of a triangle to the user in the console/terminal/output area. 
+ * 
+ * Recursive method 'makeSierpinski' edits the static variable 'triangle' to make a Sierpinski's triangle, using recursion.
+ * @param depth         an int that determines the interations of the Sierpinski's triangle.
+ * @param sideLength    an int that determines the length of the inner triangles.
+ * @param botLeftRow    an int that determines the start row of drawing the recursive, inner triangles.
+ * @param botLeftCol    an int that determines the start column of drawing the recursive, inner triangles.
+ * @return boolen       the method returns a boolean to determine when the Sierpinski triangle is finished.
+ * */
+
+
+
 import java.util.Scanner;
 
 public class Sierpinski {
-
+    
     private static char[][] triangle;
-    private static final int DEPTH = 1;
+    private static final int DEPTH = 3;
 
     public static void drawHorizontalLine(int row, int col, int length) {
         for (int i = 0; i < length; i+=2) {
@@ -66,5 +99,7 @@ public class Sierpinski {
         makeTriangle(sc);
         makeSierpinski(0, triangle.length, triangle.length, 0);
         printTriangle();
+
+        sc.close(); //@Jay I closed the scanner lol 
     }
 }
