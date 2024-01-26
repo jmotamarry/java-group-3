fac77401b484ea4fb2785a65f0483a383a18efdb
Jay Motamarry <79808791+jmotamarry@users.noreply.github.com>
2024-01-25 23:28:08 -0800

    finish without depth control

--- Sierpinski.java
+++ Sierpinski.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Sierpinski {
 
     private static char[][] triangle;
+    private static final int DEPTH = 1;
+
+    public static void drawHorizontalLine(int row, int col, int length) {
+        for (int i = 0; i < length; i+=2) {
+            triangle[row - 1][col + i] = '*';
+        }
+    }
 
     public static void makeTriangle(Scanner sc) {
         int side;
@@ -23,9 +30,7 @@ public class Sierpinski {
             triangle[row][(side * 2 - 1) / 2 - row] = '*';
         }
 
-        for (int i = 0; i < (side * 2) - 1; i+=2) {
-            triangle[side - 1][i] = '*';
-        }
+        drawHorizontalLine(triangle.length, 0, triangle[0].length);
     }
 
     public static void printTriangle() {
@@ -37,13 +42,29 @@ public class Sierpinski {
         }
     }
 
-    public static void makeSierpinski() {
-        
+    public static boolean makeSierpinski(int depth, int sideLength, int botLeftRow, int botLeftCol) {
+        sideLength /= 2;
+
+        for (int i = 0; i < sideLength; i++) {
+            triangle[botLeftRow - i - 1][botLeftCol + sideLength * 2 + i] = '*';
+            triangle[botLeftRow - i - 1][botLeftCol + sideLength * 2 - 2 - i] = '*';
+        }
+
+        drawHorizontalLine(botLeftRow - sideLength, botLeftCol + sideLength, sideLength * 2);
+
+        if (depth == DEPTH) {
+            return true;
+        }
+
+        makeSierpinski(depth + 1, sideLength, botLeftRow, botLeftCol);
+        makeSierpinski(depth + 1, sideLength, botLeftRow, botLeftCol + sideLength * 2);
+        return makeSierpinski(depth + 1, sideLength, botLeftRow - sideLength, botLeftCol + sideLength);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         makeTriangle(sc);
+        makeSierpinski(0, triangle.length, triangle.length, 0);
         printTriangle();
     }
 }
