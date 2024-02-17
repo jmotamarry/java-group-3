ec66be2074b72b45b3594d44c1d5fa7e519236ab
Jay Motamarry <79808791+jmotamarry@users.noreply.github.com>
2024-02-16 18:26:05 -0800

    make snake skeleton

--- Snake/SnakeLinkedList.java
+++ Snake/SnakeLinkedList.java
@@ -1,4 +1,69 @@
 package Snake;
+
 public class SnakeLinkedList {
-    
+    private int left;
+    private int right;
+    private int top;
+    private int bottom;
+
+    private SnakeNode head;
+
+    public SnakeLinkedList(int left, int right, int top, int bottom, SnakeNode head) {
+        this.left = left;
+        this.right = right;
+        this.top = top;
+        this.bottom = bottom;
+        this.head = head;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public SnakeNode getHead() {
+        return head;
+    }
+
+    public void setHead(SnakeNode s) {
+        head = s;
+    }
+
+    public void addAtEnd(SnakeNode s) {
+        SnakeNode curr = head;
+        while(curr.getNext() != null) {
+            curr = curr.getNext();
+        }
+        curr.setNext(s);
+        s.setPrev(curr);
+    }
+
+    public void printList() {
+        SnakeNode curr = head;
+        while(curr.getNext() != null) {
+            System.out.print("[" + curr.getRow() + ", " + curr.getCol() + "] -> ");
+            curr = curr.getNext();
+        }
+        System.out.println("[" + curr.getRow() + ", " + curr.getCol() + "]");
+    }
+
+    public static void main(String[] args) {
+        SnakeLinkedList snakeGame = new SnakeLinkedList(0, 20, 0, 20, null);
+        SnakeNode node = new SnakeNode((snakeGame.getBottom() - snakeGame.getTop()) / 2, (snakeGame.getRight() - snakeGame.getLeft()) / 2);
+        SnakeNode node2 = new SnakeNode(((snakeGame.getBottom() - snakeGame.getTop()) / 2) - 1, (snakeGame.getRight() - snakeGame.getLeft()) / 2);
+        snakeGame.setHead(node);
+        snakeGame.addAtEnd(node2);
+        snakeGame.printList();
+    }
 }
--- Snake/SnakeNode.java
+++ Snake/SnakeNode.java
@@ -1,4 +1,46 @@
 package Snake;
 public class SnakeNode {
-    
+    private SnakeNode next;
+    private SnakeNode prev;
+    private int row;
+    private int col;
+
+    public SnakeNode(int row, int col) {
+        this.row = row;
+        this.col = col;
+        next = null;
+        prev = null;
+    }
+
+    public SnakeNode getNext() {
+        return next;
+    }
+
+    public void setNext(SnakeNode s) {
+        next = s;
+    }
+
+    public SnakeNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(SnakeNode s) {
+        prev = s;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int num) {
+        row = num;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int num) {
+        col = num;
+    }
 }
